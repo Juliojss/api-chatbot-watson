83ad08183fa5bbbce3d026d40fa23048c89f391f
Júlio Saldanha <30665090+Juliojss@users.noreply.github.com>
2019-09-11 23:48:19 -0300

    Update chatbot.js

--- server/config/chatbot.js
+++ server/config/chatbot.js
@@ -1,8 +1,8 @@
 const AssistantV1 = require('watson-developer-cloud/assistant/v1');
 
 const assistant = new AssistantV1({
-    username: 'd15319f9-81cd-4de0-b4e4-daf358897938',
-    password: 'wbWO4jzGtUiy',
+    username: '',
+    password: '',
     url: 'https://gateway.watsonplatform.net/assistant/api',
     version: '2018-09-18',
 });
